fb5112c1bb71891952ec65d4bd6ae58701b7802e
vivek sharma <45270024+vvksharrma@users.noreply.github.com>
2021-01-13 00:23:06 +0530

    Update question2.cpp

--- Assignment 3/question2.cpp
+++ Assignment 3/question2.cpp
@@ -1,1 +1,32 @@
-h
+#include <stdio.h>
+int rotLeft(int arr[], int n, int d)
+{
+    int i, j;
+    int first;
+    for(i=0; i<d; i++)
+    {
+        first = arr[0];
+        for(j=0; j<n-1; j++)
+        {
+            arr[j] = arr[j+1];
+        }
+        arr[j] = first;
+    }
+    return *arr;
+}
+int main()
+{
+    int n, d, i;
+    scanf("%d",&n);
+    scanf("%d",&d);
+    int list[n];
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&list[i]);
+    }
+    rotLeft(list, n, d);
+    for(i=0; i<n; i++)
+    {
+        printf("%d ",list[i]);
+    }
+}
